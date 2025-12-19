765ebdea78e564d8dde644328fc21768e402cd02
Maxi Azis <luckiesmaxi6@gmail.com>
2025-12-20 06:35:12 +0700

    comment out drawing function (temporary)

--- websocket/websocket_client.py
+++ websocket/websocket_client.py
@@ -3,12 +3,15 @@
 """Client using the asyncio API."""
 
 import asyncio
+import json
 from websockets.asyncio.client import connect
 
 
 async def hello():
     async with connect("ws://192.168.1.43:9786") as websocket:
-        await websocket.send("Hello world!")
+        data = {"name": "ALice", "age":25}
+        to_send=json.dumps(data)
+        await websocket.send(to_send)
         message = await websocket.recv()
         print(message)
 
