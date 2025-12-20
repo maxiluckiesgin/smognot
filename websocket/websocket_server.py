98197a6f5d7e7d22f7c7454225261c11fbc0a1db
Makusi Azis <luckiesmaxi6@gmail.com>
2025-12-20 12:05:48 +0700

    receive json on websocket server

--- websocket/websocket_server.py
+++ websocket/websocket_server.py
@@ -3,16 +3,20 @@
 """Echo server using the asyncio API."""
 
 import asyncio
+import json
 from websockets.asyncio.server import serve
 
 
 async def echo(websocket):
     async for message in websocket:
-        await websocket.send(message)
+        data = json.loads(message)
+        for key,value in data.items():
+       	    print(f"{key}: {value} from client")
+        await websocket.send("Data received by server")
 
 
 async def main():
-    async with serve(echo, "localhost", 9786) as server:
+    async with serve(echo, "192.168.1.43", 9786) as server:
         await server.serve_forever()
 
 
